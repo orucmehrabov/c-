40d86f617b66612a44dc109a3ccaaf96a765cc5f
orucmehrabov <orujvm@code.edu.az>
2022-04-14 17:56:29 +0400

    first commit

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 316;
+            var sum = 0;
+
+            while ( 0 < number) ;
+
+            var reminder = number % 10;
+            sum = sum + reminder;
+            number = number / 10;
+
+            Console.WriteLine(sum);
+        }
+    }
+}

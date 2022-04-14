caa98485151afd9425a1810533c8dd79ceeb93bb
orucmehrabov <orujvm@code.edu.az>
2022-04-14 17:55:43 +0400

    first commit

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Oruc", "Orxan", "Vusal", "Arzu", "Elçin" };
+
+            foreach (string name in names);
+
+            int counter = 0;
+            foreach (char ch in names) ;
+
+            counter++;
+
+            if (counter == 5)
+
+                Console.WriteLine(names + "->" + counter + "herf");
+
+
+
+        }
+    }
+}

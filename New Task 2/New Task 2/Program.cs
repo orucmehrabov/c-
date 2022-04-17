9ba5868032a36a943155ca59f4906fafe2207e4e
orucmehrabov <orujvm@code.edu.az>
2022-04-17 13:51:25 +0400

    first commit

---
+++ New Task 2/New Task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace New_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write( "Enter the number: " );
+            int num1 = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Enter divided number: ");
+            int num2 = Convert.ToInt32 (Console.ReadLine());
+            int remainder = num1 % num2;
+
+            if (remainder == 0)
+            {
+                Console.Write("Number is divisible");
+            }
+            else if (remainder <= 3)
+            {
+                Console.Write("The nearest divisible number: " + (num1 - remainder));
+            }
+            else
+            {
+                Console.Write("The nearest divisible number: " + (num1 + num2 - remainder));
+            }
+
+
+
+        }
+    }
+}

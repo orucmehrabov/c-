5f6bbb3737802d87853ac1390cb2d379a253036b
orucmehrabov <orujvm@code.edu.az>
2022-04-17 13:52:05 +0400

    first commit

---
+++ New Task 3/New Task 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace New_Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                if (n <= 50)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else if (n > 50 && n < 100)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else if (n >= 100)
+                {
+                    if (i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+    }
+}

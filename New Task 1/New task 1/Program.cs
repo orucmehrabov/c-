ef03f977a13e6b7f18aa8d25ef7ea56e8bb7e9c6
orucmehrabov <orujvm@code.edu.az>
2022-04-17 13:50:16 +0400

    first commit

---
+++ New Task 1/New task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace New_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int number = input;
+            int counter = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                counter++;
+            }
+            Console.Write("Enter the second number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Result: ");
+            var result = a * ( Math.Pow(10, counter++)) + input ;
+            Console.Write(result );
+        }
+        
+    }
+}

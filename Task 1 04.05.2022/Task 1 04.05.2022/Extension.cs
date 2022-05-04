e867324bd7067ba99e9f07d8c76f987380577064
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2022-05-04 17:51:32 +0400

    first commit

---
+++ Task 1 04.05.2022/Task 1 04.05.2022/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1_04._05._2022
+{
+    static class Extension
+    {
+        public static void CustomContain(this string sentence, string search)
+        {
+            bool result = false;
+            string Check = string.Empty;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if ((i + search.Length) > sentence.Length)
+                {
+                    break;
+                }
+                Check = sentence.Substring(i, search.Length);
+                if (Check.ToLower() == search.ToLower())
+                {
+                    result = true;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Task 1 04.05.2022/Task 1 04.05.2022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1_04._05._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the sentence: ");
+            string sentence = Console.ReadLine();
+            Console.Write("Enter the part you are looking for: ");
+            string search = Console.ReadLine();
+            Console.Write("Result: ");
+            sentence.CustomContain(search);
+        }
+    }
+}

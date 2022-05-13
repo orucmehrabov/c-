7591d300a3443b135a52e10a78ae4412ba8e98bf
Oruj Mehrabov <102570437+orucmehrabov@users.noreply.github.com>
2022-05-13 19:15:55 +0400

    first commit

---
+++ Console Application/Console Application/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Console Application/Console Application/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Application
+{
+    class Student
+    {
+    }
+}
